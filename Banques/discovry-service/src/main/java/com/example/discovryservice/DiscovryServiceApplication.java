befc7706e3aa9f5b4d6190b9a1dcab44e77656a2
AhmedSeomaniak <ahmed.net@seomaniak.com>
2023-12-04 22:50:10 +0100

    Architecte MicroService

---
+++ Banques/discovry-service/src/main/java/com/example/discovryservice/DiscovryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.discovryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscovryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiscovryServiceApplication.class, args);
+    }
+
+}
